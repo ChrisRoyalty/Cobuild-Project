8caa7848f19b0c8706c36e83c2340f49b96973f2
jennifer-pecky <ugwojennifer7@gmail.com>
2023-07-21 13:10:56 +0100

    changed hero image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,22 @@
-
+import { BsPhone, BsHouseDoor } from 'react-icons/bs';
+import { TfiEmail } from "react-icons/tfi"
 
 export default function Footer() {
     return (
         <div>
-            <div>
+            <div className='grid grid-cols-3 mx-4'>
                 <div>
-                    <i class="bi bi-house-door"></i>
+                    <BsHouseDoor size={50} color='#f5af00' />
                     <h1>Address</h1>
                     <p>220 Petersham town, Wardell <br /> street Austrialia PA 6550.</p>
                 </div>
                 <div>
-                    icon
+                    <BsPhone size={50} color='#f5af00' />
                     <h1>Phone</h1>
                     <p>Phone:(04) 491 570 00 <br /> Fax: +04 642 021 332</p>
                 </div>
                 <div>
-                    icon
+                    <TfiEmail size={50} color='#f5af00' />
                     <h1>Email</h1>
                     <p>support@zytheme.com<br /> Twitter: @zytheme</p>
                 </div>
