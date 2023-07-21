62563d31361797a833b8f0a917981fa1e6208972
jennifer-pecky <ugwojennifer7@gmail.com>
2023-07-21 12:27:06 +0100

    changed hero image

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+
+
+export default function Footer() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <i class="bi bi-house-door"></i>
+                    <h1>Address</h1>
+                    <p>220 Petersham town, Wardell <br /> street Austrialia PA 6550.</p>
+                </div>
+                <div>
+                    icon
+                    <h1>Phone</h1>
+                    <p>Phone:(04) 491 570 00 <br /> Fax: +04 642 021 332</p>
+                </div>
+                <div>
+                    icon
+                    <h1>Email</h1>
+                    <p>support@zytheme.com<br /> Twitter: @zytheme</p>
+                </div>
+            </div>
+        </div>
+    )
+}
